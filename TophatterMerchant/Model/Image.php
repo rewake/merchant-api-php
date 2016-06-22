179aeb4ea4b7ac2e87d6266e50fb82887efe17f5
Fei Qu <feiqu0325@gmail.com>
2016-06-22 15:42:13 -0700

    Add some missing getters.

--- TophatterMerchant/Model/Image.php
+++ TophatterMerchant/Model/Image.php
@@ -8,6 +8,14 @@ class Image {
 		$this->image_hash = $image_hash;
 	}
 	
+	public function getId() {
+		return $this->image_hash->id;
+	}
+	
+	public function getFingerprint() {
+		return $this->image_hash->fingerprint;
+	}
+	
 	public function getUrl() {
 		return $this->image_hash->url;
 	}
